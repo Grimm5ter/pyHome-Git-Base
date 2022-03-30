9d62ac3f523a4bcba6f12bf9ed1ccb05c0fbd518
Grimm5ter <99831145+Grimm5ter@users.noreply.github.com>
2022-03-30 17:37:13 +0300

    pyHw40 added

---
+++ pyHw40/main.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from flask import Flask, render_template
+import db
+
+
+# Вычисляем рабочую директорию (где лежит это файл `main.py`) и задаём путь к файлу сохранения
+WORK_DIR = Path(__file__).absolute().parent
+DB_FILENAME = WORK_DIR / Path("numbers.txt")
+
+# Создание экземпляра приложения Flask
+app = Flask(__name__)
+
+# Список чисел, который загружен из файла
+list_of_integers = []
+
+
+# *** Обработчики URL ***
+
+
+@app.route("/")
+def index():
+    # Обратите внимание, как передаётся параметр `numbers` в шаблон
+    # и как в шаблоне указана подстановка значений через `{{ n }}`
+    global list_of_integers
+    list_of_integers = db.load(DB_FILENAME)
+    return render_template("index.html", numbers=list_of_integers)
+
+
+
+
+
+@app.route("/save/<number>")
+def save(number=None):
+    if number is not None:
+        db.save(DB_FILENAME, number)
+        return render_template("save_success.html", number = number)
+    return render_template("save_fail.html")
+
+
+# @app.route("/load")
+# def load():
+#     global list_of_integers
+#     list_of_integers = db.load(DB_FILENAME)
+#     return render_template("load_success.html")
